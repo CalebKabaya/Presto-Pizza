10a9b9437114f34427dc4885fb78e2e9dad27ec7
KakushaVenecia <kakushavenecia@gmail.com>
2022-05-12 14:02:05 +0300

    This will add the postgres credential

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://venecia:12345@localhost/pizza'
     DEBUG = True
 
 config_options = {
