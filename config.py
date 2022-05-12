fe35f12ed3d6df2b29a2759052ead4ec273ecd15
James Kaloki <kaloki2012@gmail.com>
2022-05-12 14:26:58 +0300

    ft: changed localURI

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ class ProdConfig(Config):
 
 
 class DevConfig(Config):
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres+psychopg2:'
     DEBUG = True
 
 config_options = {
